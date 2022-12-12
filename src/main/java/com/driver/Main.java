d8d69d9201cb4e94fd63563a8ed3fcb160e73a08
rauch <rauchrodrigues@gmail.com>
2022-12-12 15:16:48 +0530

    dfsfdw

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name="dsd";
+//        java: name has private access in com.driver.RWOnly
+        obj.setName("Rauch");
+        System.out.println(obj.getName());
+    }
 }
